ea49298e4bd706054dc00c99896450451ea4cb21
IvanCeskic <iceskic@playnirvana.com>
2026-08-22 09:18:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="display-4">Hello from Vue + Bootstrap</h1>
+    <p class="lead">Your project is ready.</p>
+    <button class="btn btn-primary">Bootstrap Button</button>
+  </div>
+</template>
+
+<script setup>
+</script>
