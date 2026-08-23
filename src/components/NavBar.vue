6c1df7e01a59c8f7a244dae58ba7c1d138729fb4
IvanCeskic <iceskic@playnirvana.com>
2026-08-23 10:26:45 +0200

    NavBar compontenta

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,40 @@
 <template>
-    <h1>Hello world</h1>
+    <div class="nav-container">
+        <img src="../images/logo.png" class="logo-img">
+        <div class="navs">
+            <RouterLink to="/dashboard">Moji računi</RouterLink>
+            <RouterLink to="/profile">Profil</RouterLink>
+            <RouterLink to="/odjava">Odjava</RouterLink>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+
+.nav-container{
+    display:flex;
+    width:100%;
+    justify-content: space-between;
+    align-items: center;
+    background-color: transparent;
+    border: 5px solid #B10B77;
+    border-radius: 24px;
+}
+.logo-img{
+  width: auto;
+  height: 50px;
+  margin: 6px 16px;
+}
+.navs{
+    display:flex;
+    align-items: center;
+    gap: 24px;
+    padding-right: clamp(16px, 3vw, 32px);
+}
+ .navs a {
+    color: #B10B77;
+    font-size: 1rem;
+    text-decoration: none;
+    font-weight: 600;
+  }
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import NavBar from '../components/NavBar.vue';
+
+</script>
+
 <template>
-    <h1>Hello world</h1>
+    <NavBar></NavBar>
 </template>
