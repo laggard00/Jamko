5787f953d91889cad5e7f94b57f0c81eb49bf480
IvanCeskic <iceskic@playnirvana.com>
2026-08-23 09:47:00 +0200

    Login page design implementation

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,21 @@
 <script setup>
 import { RouterView } from 'vue-router'
 </script>
+
+<style>
+@font-face {
+  font-family: 'CenturyGothic';
+  src: url('@/assets/fonts/centurygothic.ttf') format('truetype');
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: 'CenturyGothic';
+  src: url('@/assets/fonts/centurygothic_bold.ttf') format('truetype');
+  font-weight: 700;
+}
+
+* {
+  font-family: 'CenturyGothic', sans-serif;
+}
+</style>
